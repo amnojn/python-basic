cd0309f9854446a4d82f61545972e391116b5b0b
amnojn <aminfauzan.amnfzn@gmail.com>
2025-12-03 14:36:36 +0700

    Menambahkan file variabel.py

---
+++ variabel.py
@@ -0,0 +1,27 @@
+## Ini variabel
+nama = "Reiner Braun"
+umur = 20
+tinggi = 185
+berat = 77
+
+nama_depan = "Reiner"
+
+print(type(nama))
+print(type(umur))
+print(type(tinggi))
+print(type(berat))
+
+lyricis = """
+The silence is getting too cold
+We stopped fighting
+'Cause deep down inside, we both know
+Just for once, what do you want? Baby, tell me the truth
+Do you only love me 'cause you have to?
+"""
+
+print(type(lyricis))
+
+is_marylean = False
+is_eldian = True
+
+print(type(is_eldian))
