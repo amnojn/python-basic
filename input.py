7af0599dc571eb57c009d181c8987aa335a86469
amnojn <aminfauzan.amnfzn@gmail.com>
2025-12-03 14:50:49 +0700

    menambahkan file input.py

---
+++ input.py
@@ -0,0 +1,8 @@
+nama = input("Masukkan nama Anda: ")
+print("Halo,", nama)
+
+umur_text = input("Masukkan umur Anda: ")
+umur = int(umur_text)
+
+print("Umur Anda dalam 5 tahun akan menjadi:", umur + 5)
+print("Tipe umur", type(umur))
