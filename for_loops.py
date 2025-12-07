5b04a11b983362c74fbf8cea353ea51bc51e18b3
amnojn <aminfauzan.amnfzn@gmail.com>
2025-12-07 12:45:53 +0700

    materi for loops

---
+++ for_loops.py
@@ -0,0 +1,18 @@
+def print_numbers():
+    for i in range(100):
+        print(i)
+
+# example usage 
+
+def test():
+    print("Printing numbers from 0 to 99:")
+    print_numbers()
+    print("=====================================")
+
+
+def main():
+    test()
+
+
+main()
+
