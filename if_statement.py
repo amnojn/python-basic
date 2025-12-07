db0b418ca93cd8bca4a210396646ce93bbac78ef
amnojn <aminfauzan.amnfzn@gmail.com>
2025-12-07 09:58:06 +0700

    if statement

---
+++ if_statement.py
@@ -0,0 +1,20 @@
+def print_status(player_health):
+    if player_health <= 0:
+        print("dead")
+    print("status check complete")
+
+def test(health):
+    print(f"Player Health: {health}")
+    print("Checking status...")
+    print_status(health)
+    print("=====================================")
+
+
+def main():
+    test(0)
+    test(5)
+    test(-1)
+    test(3)
+
+
+main()
